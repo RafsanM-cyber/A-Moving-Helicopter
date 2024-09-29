8447dc5910cc907b7bcb8cc8f86145600d1d1102
Rafsan <rafsaniboi@gmail.com>
2024-09-29 19:34:23 -0400

    Add files via upload

---
+++ LolCopter/LolCopter.java
@@ -0,0 +1,56 @@
+package LolCopter;
+
+public class LolCopter {
+    public static void main(String[] args) throws InterruptedException {
+        while(true) {
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+
+         System.out.println("          :LOL:ROFL:ROFL");
+         System.out.println("           A         ");
+         System.out.println("       /----------                             ");
+         System.out.println(" LOL====        []\\    ");
+         System.out.println("  L    \\          \\     ");
+         System.out.println("        \\__________|             ");
+         System.out.println("           I       I   ");
+         System.out.println("       ---------------/          ");
+         System.out.println("                              ");
+         System.out.println("                     ");
+
+         Thread.sleep(100);
+
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+         System.out.println();
+
+         System.out.println("ROFL:ROFL:LOL:               ");
+         System.out.println("           A         ");
+         System.out.println("  L    /----------                               ");
+         System.out.println(" LOL====        []\\     ");
+         System.out.println("       \\          \\     ");
+         System.out.println("        \\__________|             ");
+         System.out.println("           I       I    ");
+         System.out.println("       ---------------/          ");
+         System.out.println("                              ");
+         System.out.println("                          ");
+
+         Thread.sleep(100);
+        }
+    }
+    
+}
